90eb6bede729eec6b016e5dd314b556873e02340
HyungwonJin <hwjin92@gmail.com>
2023-03-10 17:54:25 +0900

    refactor: Make anothor file for router

--- src/index.tsx
+++ src/index.tsx
@@ -2,24 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import Coins from "./routes/Coins";
-import Coin from "./routes/Coin";
+import {RouterProvider} from "react-router-dom";
+import router from "../Router";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Coins />
-  },
-  {
-    path: "/:coinId",
-    element: <Coin />
-  }
-])
 
 root.render(
   <React.StrictMode>
